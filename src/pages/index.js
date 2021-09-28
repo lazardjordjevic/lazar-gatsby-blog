fdb7d4bdbf4fbbe9fd373b601e455facc5e89168
lazardjordjevic <lazar.djordjevic.it@gmail.com>
2021-09-28 14:19:51 +0200

    testing integration

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,6 +6,7 @@ const IndexPage = () => {
   return (
     <main>
       <h1 class="title">Lazar's GatsbyJS Blog</h1>
+      <p>This is some description for testing integration</p>
     </main>
   )
 }
